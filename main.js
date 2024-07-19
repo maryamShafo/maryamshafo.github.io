caa373652e813e0d6c10963376ed7a4c4cc3b3de
Maryam SHAFO <maryam200008@yahoo.com>
2024-07-19 10:53:23 +0200

    css changes

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
 
-// const hamburger = document.getElementById('hamburger');
-// const navLinks = document.getElementById('nav-links');
-// const navbar = document.querySelector('.navbar');
-
-//     hamburger.addEventListener('click', () => {
-//         navbar.classList.toggle('active');
-//     });
-
-    const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('nav-links');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('active');
-        navLinks.classList.toggle('active');
-    });
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
 
 
 
